fa822d591a59c94b690b4ab6d7b4aa2c4a5afa36
Gabriel França <gabrielfrancaramos99@gmail.com>
2024-10-29 18:09:32 -0300

    aplica alguns estilos a página de login

--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
-  import { invalidateAll } from "$app/navigation"
-  import { AuthService } from "$lib/services/auth.service"
-  import type { AppwriteException } from "appwrite"
+  import { invalidateAll } from '$app/navigation'
+  import Button from '$lib/components/ui/Button.svelte'
+  import { AuthService } from '$lib/services/auth.service'
+  import type { AppwriteException } from 'appwrite'
 
   let formError: null | string = null
 
@@ -11,14 +12,11 @@
 
     const form = event.target as HTMLFormElement
     const formData = new FormData(form)
-    const email = formData.get("email") as string
-    const password = formData.get("password") as string
-
-    console.log(email)
-    console.log(password)
+    const email = formData.get('email') as string
+    const password = formData.get('password') as string
 
     if (!email || !password) {
-      formError = "Preencha todos os campos"
+      formError = 'Preencha todos os campos'
       return
     }
 
@@ -32,11 +30,20 @@
   }
 </script>
 
-<form on:submit={handleSubmit}>
-  <input type="text" name="email" placeholder="user@zerei.com" />
-  <input type="password" name="password" placeholder="⁎⁎⁎⁎⁎⁎⁎⁎" />
-  <button type="submit">Entrar</button>
-  <a href="/recuperar-senha">Recuperar senha</a>
+<form on:submit={handleSubmit} class="grid p-6 gap-4">
+  <input
+    type="text"
+    name="email"
+    placeholder="user@zerei.com"
+    class="px-4 py-2 rounded-md bg-transparent border border-slate-dark-6"
+  />
+  <input
+    type="password"
+    name="password"
+    placeholder="⁎⁎⁎⁎⁎⁎⁎⁎"
+    class="px-4 py-2 rounded-md bg-transparent border border-slate-dark-6"
+  />
+  <Button ariaLabel="Acessar o Zerei" type="submit">Entrar</Button>
 
   {#if formError}
     <p>{formError}</p>
