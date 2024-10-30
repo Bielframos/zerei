c4369faf50354d53b6204cd6a8dbce321d929b7a
Gabriel França <gabrielfrancaramos99@gmail.com>
2024-10-30 18:45:37 -0300

    esconde botão do menu para usuários não autenticados

--- src/lib/components/modules/Header.svelte
+++ src/lib/components/modules/Header.svelte
@@ -2,6 +2,8 @@
   import { ArrowLeft } from 'lucide-svelte'
   import { Button } from '../ui'
   import Menu from './Menu.svelte'
+  import cn from '$lib/utils/cn'
+  import { buttonStyle } from '../ui/Button.svelte'
 
   const { account, back }: { account: Account; back?: boolean } = $props()
 
@@ -22,5 +24,20 @@
     {/if}
     <h1 class="text-3xl font-normal">Zerei</h1>
   </div>
-  <Menu {account} />
+
+  {#if account}
+    <Menu {account} />
+  {:else}
+    <div class="flex gap-2 items-center">
+      <a
+        href="/auth/login"
+        class={cn(buttonStyle({ size: 'small', variant: 'secondary' }))}
+        >Acessar
+      </a>
+
+      <a href="/auth/registrar" class={cn(buttonStyle({ size: 'small' }))}
+        >Criar conta
+      </a>
+    </div>
+  {/if}
 </header>
