5b899f685111896eb8e185f9789d1d8a77caba55
Gabriel França <gabrielfrancaramos99@gmail.com>
2024-10-30 16:03:44 -0300

    adiciona botão de voltar no componente `header`

--- src/lib/components/modules/Header.svelte
+++ src/lib/components/modules/Header.svelte
@@ -1,10 +1,26 @@
 <script lang="ts">
+  import { ArrowLeft } from 'lucide-svelte'
+  import { Button } from '../ui'
   import Menu from './Menu.svelte'
 
-  const { account }: { account: Account } = $props()
+  const { account, back }: { account: Account; back?: boolean } = $props()
+
+  function backToLastPage() {
+    window.history.back()
+  }
 </script>
 
 <header class="pt-10 pb-4 px-6 flex justify-between">
-  <h1 class="text-3xl font-normal">Zerei</h1>
+  <div class="flex gap-4 items-center">
+    {#if back}
+      <Button
+        ariaLabel="Limpar busca"
+        variant="secondary"
+        size="icon"
+        onclick={() => backToLastPage()}><ArrowLeft /></Button
+      >
+    {/if}
+    <h1 class="text-3xl font-normal">Zerei</h1>
+  </div>
   <Menu {account} />
 </header>
