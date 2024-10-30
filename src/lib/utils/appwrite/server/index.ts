6fbbc73fdf1b899f92bd1e5ba811cf70dfb71dac
Gabriel França <gabrielfrancaramos99@gmail.com>
2024-10-30 01:28:46 -0300

    expande `server client`do appwrite

--- src/lib/utils/appwrite/server/index.ts
+++ src/lib/utils/appwrite/server/index.ts
@@ -1,45 +1,48 @@
-import { Client, Account, Databases } from "node-appwrite"
-import { APPWRITE_KEY } from "$env/static/private"
+import { Client, Account, Databases, Functions } from 'node-appwrite'
+import { APPWRITE_KEY } from '$env/static/private'
 import {
-	PUBLIC_APPWRITE_ENDPOINT,
-	PUBLIC_APPWRITE_PROJECT,
-} from "$env/static/public"
-import type { RequestEvent } from "@sveltejs/kit"
+  PUBLIC_APPWRITE_ENDPOINT,
+  PUBLIC_APPWRITE_PROJECT,
+} from '$env/static/public'
+import type { RequestEvent } from '@sveltejs/kit'
 
-export const SESSION_COOKIE = "session"
+export const SESSION_COOKIE = 'session'
 
 export function createAdminClient() {
-	const client = new Client()
-		.setEndpoint(PUBLIC_APPWRITE_ENDPOINT)
-		.setProject(PUBLIC_APPWRITE_PROJECT)
-		.setKey(APPWRITE_KEY)
+  const client = new Client()
+    .setEndpoint(PUBLIC_APPWRITE_ENDPOINT)
+    .setProject(PUBLIC_APPWRITE_PROJECT)
+    .setKey(APPWRITE_KEY)
 
-	return {
-		get account() {
-			return new Account(client)
-		},
-		get databases() {
-			return new Databases(client)
-		},
-	}
+  return {
+    get account() {
+      return new Account(client)
+    },
+    get databases() {
+      return new Databases(client)
+    },
+    get functions() {
+      return new Functions(client)
+    },
+  }
 }
 
 export function createSessionClient(event: RequestEvent) {
-	const client = new Client()
-		.setEndpoint(PUBLIC_APPWRITE_ENDPOINT)
-		.setProject(PUBLIC_APPWRITE_PROJECT)
+  const client = new Client()
+    .setEndpoint(PUBLIC_APPWRITE_ENDPOINT)
+    .setProject(PUBLIC_APPWRITE_PROJECT)
 
-	const session = event.cookies.get(SESSION_COOKIE)
+  const session = event.cookies.get(SESSION_COOKIE)
 
-	if (!session) {
-		throw new Error("No user session")
-	}
+  if (!session) {
+    throw new Error('No user session')
+  }
 
-	client.setSession(session)
+  client.setSession(session)
 
-	return {
-		get account() {
-			return new Account(client)
-		},
-	}
+  return {
+    get account() {
+      return new Account(client)
+    },
+  }
 }
