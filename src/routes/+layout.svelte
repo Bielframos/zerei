ee1ab8bdcc429cac13f9cab311b8a7b299d91282
Gabriel França <gabrielfrancaramos99@gmail.com>
2024-10-30 18:47:12 -0300

    corrige bug na rolagem da página

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,9 +3,14 @@
   import '../app.css'
   import { Toast } from '$lib/components/ui'
   import ZereiTeamSheet from '$lib/components/modules/ZereiTeamSheet.svelte'
+  import { onNavigate } from '$app/navigation'
 
   let { data, ...props } = $props()
   let account = $derived(data.account)
+
+  onNavigate(() => {
+    document.body.classList.remove('overflow-hidden')
+  })
 </script>
 
 <Toast />
