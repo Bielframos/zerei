17b08a2cce702f9fa16dbe11ae5dd0cf22bb441b
Gabriel França <gabrielfrancaramos99@gmail.com>
2024-10-30 01:27:47 -0300

    implementa recurso para busca de jogos populares e corrige recurso de buscar um único game no IGDB

--- src/lib/services/igdb.service.ts
+++ src/lib/services/igdb.service.ts
@@ -1,29 +1,40 @@
-import { appwrite } from "$lib/utils/appwrite/client"
-import { ExecutionMethod } from "appwrite"
+import { appwrite } from '$lib/utils/appwrite/client'
+import { ExecutionMethod } from 'appwrite'
 
-const IGDB_FUNCTION_ID = "6720f6ee0017b924e933"
+const IGDB_FUNCTION_ID = '6720f6ee0017b924e933'
 
 export const IGDBServices = {
-	find: async (query: string) => {
-		const result = await appwrite.functions.createExecution(
-			IGDB_FUNCTION_ID,
-			JSON.stringify({ query }),
-			false,
-			"/game/find",
-			ExecutionMethod.POST,
-		)
+  find: async (query: string) => {
+    const result = await appwrite.functions.createExecution(
+      IGDB_FUNCTION_ID,
+      JSON.stringify({ query }),
+      false,
+      '/find',
+      ExecutionMethod.POST
+    )
 
-		return JSON.parse(result.responseBody) as CustomGameFromGamesApi[]
-	},
-	getUnique: async (id: number) => {
-		const result = await appwrite.functions.createExecution(
-			IGDB_FUNCTION_ID,
-			JSON.stringify({ query: id }),
-			false,
-			"/game/get-unique",
-			ExecutionMethod.POST,
-		)
+    return JSON.parse(result.responseBody) as SummaryGameFromIGDB[]
+  },
+  getMostPopular: async () => {
+    const result = await appwrite.functions.createExecution(
+      IGDB_FUNCTION_ID,
+      '',
+      false,
+      '/most-popular',
+      ExecutionMethod.POST
+    )
 
-		return JSON.parse(result.responseBody) as GameFromGamesApi
-	},
+    return JSON.parse(result.responseBody) as SummaryGameFromIGDB[]
+  },
+  getUnique: async (id: string) => {
+    const result = await appwrite.functions.createExecution(
+      IGDB_FUNCTION_ID,
+      JSON.stringify({ query: id }),
+      false,
+      '/get-unique',
+      ExecutionMethod.POST
+    )
+
+    return JSON.parse(result.responseBody) as GameFromIGDB
+  },
 }
