932f9b5b55b6366a362d30712d7ff8e147e1f72d
Gabriel França <gabrielfrancaramos99@gmail.com>
2024-10-30 01:26:46 -0300

    implementa recurso para busca de games no Appwrite

--- src/lib/controllers/game.controller.ts
+++ src/lib/controllers/game.controller.ts
@@ -1,21 +1,46 @@
-import { gamesService } from "$lib/services/game.service"
+import { invalidate } from '$app/navigation'
+import { gamesService } from '$lib/services/game.service'
 
 export const GameController = {
-	createGame: async (game: GameFromGamesApi) => {
-		try {
-			const createdGame = await gamesService.create(game)
-			return createdGame
-		} catch (error) {
-			console.error("Error creating game:", error)
-			throw error
-		}
-	},
-	deleteGame: async (id: string) => {
-		try {
-			await gamesService.delete(id)
-		} catch (error) {
-			console.error("Error deleting game:", error)
-			throw error
-		}
-	},
+  createGame: async (game: GameFromIGDB) => {
+    try {
+      const createdGame = await gamesService.create(game)
+      invalidate('data:games')
+      return createdGame
+    } catch (error) {
+      console.error('Error creating game:', error)
+      throw error
+    }
+  },
+  deleteGame: async (id: string) => {
+    try {
+      await gamesService.delete(id)
+    } catch (error) {
+      console.error('Error deleting game:', error)
+      throw error
+    }
+  },
+  getUniqueGame: async (by: 'id' | 'externalReference', query: string) => {
+    try {
+      const games = await gamesService.getUnique(by, query)
+
+      if (games.total > 0) {
+        return games.documents[0]
+      }
+
+      return null
+    } catch (error) {
+      console.error('Error searching for game:', error)
+      throw error
+    }
+  },
+  findGames: async (query: string) => {
+    try {
+      const games = await gamesService.findGames(query)
+      return games
+    } catch (error) {
+      console.error('Error searching for game:', error)
+      throw error
+    }
+  },
 }
