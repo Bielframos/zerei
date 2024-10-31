72a2ea999e3487fe693796be9aea8a70250c762e
Gabriel França <gabrielfrancaramos99@gmail.com>
2024-10-31 16:36:04 -0300

    aprimora componente de loading para a visualização nos modais

--- src/lib/components/modules/Loading.svelte
+++ src/lib/components/modules/Loading.svelte
@@ -2,6 +2,6 @@
   import { LoaderCircle } from 'lucide-svelte'
 </script>
 
-<div class="flex justify-center items-center py-6">
+<div class="flex justify-center items-center py-6 col-span-2">
   <LoaderCircle size={32} class="animate-spin text-slate-dark-11" />
 </div>
