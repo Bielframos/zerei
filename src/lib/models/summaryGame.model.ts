5d0d0174d924607e55e626b1d8cf5bd57997e955
Gabriel França <gabrielfrancaramos99@gmail.com>
2024-10-31 16:39:51 -0300

    aprimora o modelo `summaryGame`

--- src/lib/models/summaryGame.model.ts
+++ src/lib/models/summaryGame.model.ts
@@ -3,47 +3,17 @@ import { imageUrlBuilder } from '$lib/utils/igdb'
 export class IGDBSummaryGame {
   id: string
   name: string
-  slug: string
   private cover?: { id: number; image_id: string }
-  private firstReleaseDate: number
 
   constructor(game: SummaryGameFromIGDB) {
     this.id = String(game.id)
     this.name = game.name
-    this.slug = game.slug
     this.cover = game.cover
-    this.firstReleaseDate = game.first_release_date
   }
 
   get coverImageUrl() {
     if (this.cover) {
       return imageUrlBuilder(this.cover.image_id, 'cover_big')
     }
-  }
-
-  get launchYear() {
-    const today = new Date(this.firstReleaseDate * 1000)
-    return today.getFullYear()
-  }
-}
-
-export class ZereiSummaryGame {
-  id: string
-  name: string
-  slug: string
-  coverImageUrl: string
-  private releaseDate: string
-
-  constructor(game: GameZerei) {
-    this.id = game.$id
-    this.name = game.name
-    this.slug = game.slug
-    this.coverImageUrl = game.coverImage
-    this.releaseDate = game.releaseDate
-  }
-
-  get launchYear() {
-    const today = new Date(this.releaseDate)
-    return today.getFullYear()
   }
 }
